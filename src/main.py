632987bc42600986ff2b5a427a752deb84b41c30
nwillhite <nwill016@ucr.edu>
2017-12-15 03:13:10 -0800

    moving printing of passphrase to rand.py, fixed spelling error

--- src/main.py
+++ src/main.py
@@ -8,18 +8,11 @@ rolldict = {'1': 5,'2': 10, '3': 15, '4': 20,'5': 25, '6': 30,'7': 35,
             '8': 40, '9': 45,'10': 50}
 
 """  Runs program  """
-
 # choice of number of words to be generated
 uservalue = menu_choice(rolldict)
 
-# generates passpharse based on number of rolls that
-rolled_dice = pharse_roll(uservalue)
+# generates passphrase based on number of rolls that
+phrase_roll(uservalue)
 
-# will print the dice numbers rolled to be used for the passpharse
-print_dice_roll(rolled_dice)
-
-# prints passpharse
-print_passphare(passpharse)
-
-# choice of adding random characters to passpharse
+# choice of adding random characters to passphrase
 generate_rand_char()
